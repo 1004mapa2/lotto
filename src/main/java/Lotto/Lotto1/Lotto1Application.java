ae96b9808c33e2d62e198c140562eecf72c2acec
phj <1004mapa2@naver.com>
2023-06-05 11:35:46 +0900

    기본 파일 커밋

---
+++ src/main/java/Lotto/Lotto1/Lotto1Application.java
@@ -0,0 +1,13 @@
+package Lotto.Lotto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lotto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lotto1Application.class, args);
+	}
+
+}
---
+++ src/test/java/Lotto/Lotto1/Lotto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Lotto.Lotto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lotto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
