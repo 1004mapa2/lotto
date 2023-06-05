bb152fffbb814d015dc0ee4aa375618e2c36dcfb
phj <1004mapa2@naver.com>
2023-06-05 11:44:33 +0900

    도메인 추가

---
+++ src/main/java/Lotto/Lotto1/domain/LottoDomain.java
@@ -0,0 +1,17 @@
+package Lotto.Lotto1.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LottoDomain {
+
+//    private static int[] screen = new int[6];
+    private Integer screenNumber1;
+    private Integer screenNumber2;
+    private Integer screenNumber3;
+    private Integer screenNumber4;
+    private Integer screenNumber5;
+    private Integer screenNumber6;
+}
