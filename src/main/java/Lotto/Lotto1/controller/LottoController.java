330f123a7ec27350faaabd99f48e8a32e6141b84
phj <1004mapa2@naver.com>
2023-06-06 21:46:20 +0900

    컨트롤러 수정

--- src/main/java/Lotto/Lotto1/controller/LottoController.java
+++ src/main/java/Lotto/Lotto1/controller/LottoController.java
@@ -22,17 +22,25 @@ public class LottoController {
         Map<String, Integer> getStore1 = lottoService.lottoStore1Get();
         Map<String, Integer> getStore2 = lottoService.lottoStore2Get();
         Map<String, Integer> getStore3 = lottoService.lottoStore3Get();
+        Map<String, Integer> getStore4 = lottoService.lottoStore4Get();
+        Map<String, Integer> getStore5 = lottoService.lottoStore5Get();
         model.addAttribute("getStore1", getStore1);
         model.addAttribute("getStore2", getStore2);
         model.addAttribute("getStore3", getStore3);
+        model.addAttribute("getStore4", getStore4);
+        model.addAttribute("getStore5", getStore5);
 
-        return "index";
+        return "/index";
     }
 
     @GetMapping("/pull")
     public String pull() {
-
         LottoDomain lottoDomain = lottoService.LottoNumberCreate();
+        lottoService.LottoStore8Save();
+        lottoService.LottoStore7Save();
+        lottoService.LottoStore6Save();
+        lottoService.LottoStore5Save();
+        lottoService.LottoStore4Save();
         lottoService.LottoStore3Save();
         lottoService.LottoStore2Save();
         lottoService.LottoStore1Save(lottoDomain);
@@ -43,7 +51,27 @@ public class LottoController {
     @GetMapping("/clear")
     public String clear() {
         lottoService.lottoStoreClear();
+        return "redirect:/list";
+    }
 
-        return "redirect:/";
+    @GetMapping("/list")
+    public String list(Model model) {
+        Map<String, Integer> getStore1 = lottoService.lottoStore1Get();
+        Map<String, Integer> getStore2 = lottoService.lottoStore2Get();
+        Map<String, Integer> getStore3 = lottoService.lottoStore3Get();
+        Map<String, Integer> getStore4 = lottoService.lottoStore4Get();
+        Map<String, Integer> getStore5 = lottoService.lottoStore5Get();
+        Map<String, Integer> getStore6 = lottoService.lottoStore6Get();
+        Map<String, Integer> getStore7 = lottoService.lottoStore7Get();
+        Map<String, Integer> getStore8 = lottoService.lottoStore8Get();
+        model.addAttribute("getStore1", getStore1);
+        model.addAttribute("getStore2", getStore2);
+        model.addAttribute("getStore3", getStore3);
+        model.addAttribute("getStore4", getStore4);
+        model.addAttribute("getStore5", getStore5);
+        model.addAttribute("getStore6", getStore6);
+        model.addAttribute("getStore7", getStore7);
+        model.addAttribute("getStore8", getStore8);
+        return "/list";
     }
 }
